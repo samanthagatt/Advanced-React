1673906619068dec5b925b454fc6d12d287b1de8
Samantha Gatt <smfgatt@gmail.com>
2019-01-11 22:09:46 -0500

    Adds styled components practice

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,6 +1,17 @@
 import React, {Component} from "react";
 import Header from "./Header";
 import Meta from "./Meta";
+import styled from "styled-components";
+
+// Tag template literal
+const MyButton = styled.button`
+    background: red;
+    font-size: ${props => props.huge ? "100px" : "50px"};
+    
+    span {
+        font-size: 100px;
+    }
+`;
 
 export default class Page extends Component {
     render() {
@@ -8,6 +19,14 @@ export default class Page extends Component {
             <div>
                 <Meta />
                 <Header />
+                <MyButton huge>
+                    Click Me
+                    <span>!</span>
+                </MyButton>
+                <MyButton>
+                    Click Me
+                    <span>!</span>
+                </MyButton>
                 {this.props.children}
             </div>
         )
