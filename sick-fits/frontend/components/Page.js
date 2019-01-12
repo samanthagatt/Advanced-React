a0a2813c1b7105e41e7c2097c4fe0f620a59b758
Samantha Gatt <smfgatt@gmail.com>
2019-01-11 23:30:19 -0500

    Styles navigation bar

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
+import styled, {ThemeProvider, injectGlobal} from "styled-components";
 import Header from "./Header";
 import Meta from "./Meta";
-import styled, {ThemeProvider, injectGlobal} from "styled-components";
 
 const theme = {
     red: "#FF0000",
@@ -13,8 +13,10 @@ const theme = {
     // box shadow
     bs: "0 12px 24px 0 rgba(0, 0, 0, 0.09)"
 }
-theme.gray = theme.grey;
 theme.lightGray = theme.lightgrey;
+theme.lightgrey = theme.lightGray;
+theme.gray = theme.grey;
+theme.grey = theme.gray;
 
 const StyledPage = styled.div`
     background: white;
@@ -26,6 +28,34 @@ const Inner = styled.div`
     margin: 0 auto;
     padding: 2rem;
 `;
+
+injectGlobal`
+    @font-face {
+        font-family: "radnika_next";
+        src: url("/static/radnikanext-medium-webfont.woff2") format("woff2");
+        font-weight: normal;
+        font-style: normal;
+    }
+    html {
+        font-family: "radnika_next";
+        box-sizing: border-box;
+        font-size: 10px;
+    }
+    *, *:before, *:after {
+        box-sizing: inherit;
+    }
+    body {
+        padding: 0;
+        margin: 0;
+        font-size: 1.5rem;
+        line-height: 2;
+    }
+    a {
+        text-decoration: none;
+        /* Can't access props.theme since we aren't in ThemeProvider yet */
+        color: ${theme.black};
+    }
+`
 
 export default class Page extends Component {
     render() {
--- sick-fits/frontend/components/styles/NavStyles.js
+++ sick-fits/frontend/components/styles/NavStyles.js
@@ -25,7 +25,7 @@ const NavStyles = styled.ul`
     &:before {
       content: '';
       width: 2px;
-      background: ${props => props.theme.lightgrey};
+      background: ${props => props.theme.black};
       height: 100%;
       left: 0;
       position: absolute;
